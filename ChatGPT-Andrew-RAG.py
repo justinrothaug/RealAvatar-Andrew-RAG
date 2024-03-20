026fea5d5ec9b747e65895d7395376eb2cfcd96b
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-03-19 19:07:29 -0700

    Update ChatGPT-Andrew-RAG.py

--- ChatGPT-Andrew-RAG.py
+++ ChatGPT-Andrew-RAG.py
@@ -15,7 +15,7 @@ client = OpenAI(api_key= st.secrets["openai_key"])
 def get_chatassistant_chain():
     loader = CSVLoader(file_path="./docs/DLScripts.csv")
     documents = loader.load()
-    embeddings_model = OpenAIEmbeddings(openai.api_key= client)
+    embeddings_model = OpenAIEmbeddings(client)
     vectorstore = FAISS.from_documents(documents, embeddings_model)
     llm = ChatOpenAI(model="ft:gpt-3.5-turbo-0125:personal::93Td8brn", temperature=1)
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True) 
