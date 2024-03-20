fc7cf1d03eac6753d49e70c9a32cf7a83f73d17e
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-03-19 18:33:33 -0700

    Add files via upload

---
+++ ChatGPT-Andrew-Prompts.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+#from decouple import config
+from langchain.memory import ConversationBufferWindowMemory
+from openai import OpenAI
+
+st.set_page_config(page_title="Andrew Ng")
+
+assistant_logo = 'https://pbs.twimg.com/profile_images/733174243714682880/oyG30NEH_400x400.jpg'
+client = OpenAI(api_key = st.secrets["openai_key"])
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "question"],
+    template="""You are Andrew Ng, a Chinese-American computer scientist focusing on machine learning and AI. 
+    Respond as Andrew in a friendly but professional tone, with at least a few sentences but a maximum of 128 words.
+    Do not talk about politics, or speak with curse words.
+    
+    chat_history: {chat_history},
+    Human: {question}
+    AI:"""
+)
+
+llm = ChatOpenAI(openai_api_key = st.secrets["openai_api_key"])
+memory = ConversationBufferWindowMemory(memory_key="chat_history", k=4)
+llm_chain = LLMChain(
+    llm = ChatOpenAI(model="ft:gpt-3.5-turbo-0125:personal::93Td8brn", temperature=0),
+    memory=memory,
+    prompt=prompt
+)
+
+
+st.title("Andrew Ng")
+
+
+# check for messages in session and create if not exists
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Hello, I'm Andrew! 👋"}
+    ]
+
+
+# Display all messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+user_prompt = st.chat_input()
+
+if user_prompt is not None:
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+    with st.chat_message("user"):
+        st.write(user_prompt)
+        
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant", avatar=assistant_logo):
+        with st.spinner("Loading..."):
+            ai_response = llm_chain.predict(question=user_prompt)
+            st.write(ai_response)
+    new_ai_message = {"role": "assistant", "content": ai_response}
+    st.session_state.messages.append(new_ai_message)
---
+++ ChatGPT-Andrew-RAG.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.callbacks import get_openai_callback
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain_openai import OpenAI
+openai_api_key=st.secrets['api_key']
+
+def get_chatassistant_chain():
+    loader = CSVLoader(file_path="./docs/DLScripts.csv")
+    documents = loader.load()
+    embeddings_model = OpenAIEmbeddings(openai_api_key= openai_api_key)
+    vectorstore = FAISS.from_documents(documents, embeddings_model)
+    llm = ChatOpenAI(model="ft:gpt-3.5-turbo-0125:personal::93Td8brn", temperature=1)
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True) 
+    chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(openai_api_key),
+                                                  retriever=vectorstore.as_retriever(),
+                                                  memory=memory)                                      
+    return chain
+
+
+chain = get_chatassistant_chain()
+
+assistant_logo = 'https://pbs.twimg.com/profile_images/733174243714682880/oyG30NEH_400x400.jpg'
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "ft:gpt-3.5-turbo-0125:personal::93Td8brn"
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "system", "content": "You are Andrew Ng, a Chinese-American computer scientist focusing on machine learning and AI. You have a wife named Carol, and two children. Respond to the following lines of dialog as Andrew Ng"}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+
+    with st.chat_message("assistant", avatar=assistant_logo):
+        message_placeholder = st.empty()
+        response = chain.invoke({"question": prompt})
+        message_placeholder.markdown(response['answer'])
+
+    st.session_state.messages.append({"role": "assistant", "content": response['answer']})
