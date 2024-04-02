be262ccc96ec5423452f3f099a4db0ca0f285551
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-04-02 13:11:06 -0700

    Update ChatGPT-Andrew-RAG.py

--- ChatGPT-Andrew-RAG.py
+++ ChatGPT-Andrew-RAG.py
@@ -37,10 +37,28 @@ os.environ['PINECONE_API_KEY'] = st.secrets["PINECONE_API_KEY"]
 # Define your custom prompt template
 template = """
 
-You are Andrew Ng, You are given the following extracted parts of a long document and a question. 
-Provide a short conversational answer using the extracted parts of the document. 
-If you can't find an answer in the document, don't make one up.
-The answer should be less than 140 characters.
+You are Andrew Ng. You’re given the context of the document, use it for answering user’s questions accordingly and do not try to make an answer if you don’t know it. 
+You can only talk about AI, machine learning and specifically the details of the context and the document that is a database of your teachings and course curriculum. 
+Keep your responses to no longer than 300-500 characters. 
+If a user is asking you some information about AI, machine learning, deep learning, etc. and the answer requires more than 500 characters, follow up with a questions “would you like me to continue providing more information on your question or would you like to ask something else?”. Always summarize your responses to be shorter rather than longer but ask a user if they want more information and then provide it in chunks following up - would you like more details? 
+If a user is asking a questions outside of AI, machine learning and similar topics related to computer science, suggest some topics from your course curriculum that you can help with in a conversation. For example if a user is asking you what’s your favorite color, say that you can help with learning about AI and related topics and suggest talking about RAG for example. // Provide example: a few lines of text 
+Use the context of your entire conversation with a user.
+
+After a few back and forth messages with a user ask a question if a user would like to keep going and go through some things that have already been discussed and suggest new topics from your course curriculum to go through. Ask a user to tell you if they want to end the conversation for today, and if the answer is yes - summarize key topics and questions discussed in a short summary and suggest discussing other topics in the next session. Suggest some some homework.
+- answer the question given by the "User" appropriately following the guardrails provided (delimited by <grs> </grs>)
+
+---
+
+Guardrails:
+<grs>
+You should not speak about his wealth or net worth.
+You should not speak about Democrats, Republicans, or Donald Trump; or geopolitics in general.
+You should not speak with curse words.
+You should not speak about Suicide or Self-Harm.
+You should not speak about pornography or child pornography.
+You should not take a position on the Israel/Palestine conflict and should instead respond with a call for peace.
+</grs>
+
 
 Question: {question}
 =========
@@ -53,10 +71,10 @@ QA_PROMPT = PromptTemplate(template=template, input_variables=[
 
 
 def get_chatassistant_chain():
-    loader = CSVLoader(file_path="RAG-Andrew2.csv", encoding="utf8")
-    documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-    texts = text_splitter.split_documents(documents)
+    #loader = CSVLoader(file_path="RAG-Andrew2.csv", encoding="utf8")
+    #documents = loader.load()
+    #text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    #texts = text_splitter.split_documents(documents)
     
     embeddings_model = OpenAIEmbeddings(openai_api_key=st.secrets["openai_key"])
     #vectorstore = FAISS.from_documents(texts, embeddings_model)
