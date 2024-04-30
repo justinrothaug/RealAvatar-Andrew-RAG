a740d227baf078bb2fe25e41c77188ff7dea03cd
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-04-30 10:36:38 -0700

    Update ChatGPT-Andrew-RAG.py

--- ChatGPT-Andrew-RAG.py
+++ ChatGPT-Andrew-RAG.py
@@ -50,6 +50,11 @@ ELEVEN_LABS_API_KEY= os.environ["ELEVEN_LABS_API_KEY"]
 client2= ElevenLabs(api_key= os.environ["ELEVEN_LABS_API_KEY"])
 GRADIENT_ACCESS_TOKEN=os.environ["GRADIENT_ACCESS_TOKEN"]
 GRADIENT_WORKSPACE_ID=os.environ["GRADIENT_WORKSPACE_ID"]
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_ENDPOINT"]="https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"]="ls__f58fcca57d5b430998efab563129b779"
+os.environ["LANGCHAIN_PROJECT"]="pt-uncommon-nexus-100"
+load_dotenv(override=True)
 
 #Set up the Environment
 st.set_page_config(page_title="Andrew AI", layout="wide")
